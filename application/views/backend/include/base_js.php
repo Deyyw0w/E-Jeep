30b219e255d0577e3183c40ff68687c8373b6db6
Deyyw0w <Deyyw0w>
2024-03-15 19:47:59 +0800

    modal changes

--- application/views/backend/include/base_js.php
+++ application/views/backend/include/base_js.php
@@ -35,13 +35,13 @@
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel">Tentang MANTIK</h5>
+				<h5 class="modal-title" id="exampleModalLabel">HATID MOKO</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
 			<div class="modal-body">
-				BY ULTIMATE GROUP
+				BY BSIT-3A
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
